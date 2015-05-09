65ef2b13cfac37f32361056734b46570cdc0e87c
crashingdaily <mheiges@uga.edu>
2015-05-09 14:32:43 -0400

    set maximum input size

--- rpc_client.pl
+++ rpc_client.pl
@@ -9,9 +9,17 @@ use strict;
 use Net::RabbitMQ;
 use Data::Dumper;
 
+my $max_input = 35;
+
 my $host = 'localhost';
 my $channel_id = 1;
 my $routing_key = 'rpc_queue';
+my $input = $ARGV[0] || 30;
+
+if ($input > $max_input) {
+  print "Maximum input value is $max_input.\n";
+  exit 1;
+}
 
 my $mq = Net::RabbitMQ->new() ;
 
@@ -22,23 +30,21 @@ $mq->channel_open($channel_id);
 my $queue_name = $mq->queue_declare($channel_id, '', { exclusive => 1 });
 
 
-print " [x] Requesting fib(30)\n";
-my $response = call(30);
-print " [.] Got $response\n";
+print " [x] Requesting fib($input)\n";
+my $response = call($input);
+print " [.] Got fib($input) is $response\n";
 
 
 sub call {
   my ($n) = @_;
   my $correlation_id = `uuidgen`;
   $mq->publish($channel_id, $routing_key, $n, {}, {reply_to => $queue_name, correlation_id => $correlation_id});
-  print "publish $n to queue $queue_name, routing_key $routing_key, correlation_id $correlation_id\n";
+  #print "publish $n to queue $queue_name, routing_key $routing_key, correlation_id $correlation_id\n";
   my $payload;
   my $response_correlation_id;
   do {
-    sleep 1;
     $payload = $mq->get($channel_id, $queue_name);
     $response_correlation_id = $payload->{'props'}{'correlation_id'};
   } while ($payload == undef || ($correlation_id ne $response_correlation_id));
-  my $response = $payload->{'body'};
-  return $response;
+  return $payload->{'body'};
 }
